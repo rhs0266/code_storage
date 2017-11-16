a3c683ef99f607b5fea1a0e09aaae54be97c4bfa
rhs0266 <dkflfkdeh@naver.com>
2017-11-16 13:15:02 +0900

    Update autumata_HW2.cpp

--- autumata_HW2.cpp
+++ autumata_HW2.cpp
@@ -21,11 +21,14 @@ struct STATE{
     vector<EDGE> edges;
     STATE(){}
     STATE(int id, bool fs, bool ds): id(id),fs(fs),ds(ds){}
+    void push_edge(int to, char inp, char top, char _push[]){
+        edges.push_back(EDGE(to, inp, top, _push));
+    }
 };
 
 struct DPA{
     vector<STATE> states;
-    int init, final;
+    int init, waypoint, final;
     char vars[]="EeTtFA";
     char alphabets[]="0+-*/()#";
     char table[6][8][]={{"Te","","","","","Te","",""},
@@ -37,6 +40,21 @@ struct DPA{
     DPA(){ init=6; final=7; }
     
     void makeDPA(){
+        int nvars = strlen(vars);
+        int nalphabets = strlen(alphabets);
+        
+        init = nalphabets; waypoint = nalphabets+1; final=nalphabets+2;
+        
+        states.resize(nalphabets+3);
+        
+        for (int i=0;i<nvars;i++){
+            for (int j=0;j<nalphabets;j++){
+               if (strcmp(table[i][j],"")==0){
+                   continue;
+               }
+               states[j].push_edge(j, eps, vars[i], table[i][j]);
+            }
+        }
     }
     
     bool read(char* inp){
